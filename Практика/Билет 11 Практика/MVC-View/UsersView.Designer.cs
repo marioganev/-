b2e99571179b66d9432fc93d14ffe564d33bef28
Mario Ganev <82400980+marioganev@users.noreply.github.com>
2024-05-09 13:36:30 +0300

    Add files via upload

---
+++ Практика/Билет 11 Практика/MVC-View/UsersView.Designer.cs
@@ -0,0 +1,250 @@
+﻿
+namespace MVC_View
+{
+    partial class UsersView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpDetails = new System.Windows.Forms.GroupBox();
+            this.grbSex = new System.Windows.Forms.GroupBox();
+            this.rdMale = new System.Windows.Forms.RadioButton();
+            this.rdFamele = new System.Windows.Forms.RadioButton();
+            this.txtDepartment = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.lblDepartment = new System.Windows.Forms.Label();
+            this.lblID = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.grdUsers = new System.Windows.Forms.ListView();
+            this.grpDetails.SuspendLayout();
+            this.grbSex.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpDetails
+            // 
+            this.grpDetails.Controls.Add(this.grbSex);
+            this.grpDetails.Controls.Add(this.txtDepartment);
+            this.grpDetails.Controls.Add(this.txtID);
+            this.grpDetails.Controls.Add(this.txtLastName);
+            this.grpDetails.Controls.Add(this.txtFirstName);
+            this.grpDetails.Controls.Add(this.lblDepartment);
+            this.grpDetails.Controls.Add(this.lblID);
+            this.grpDetails.Controls.Add(this.lblLastName);
+            this.grpDetails.Controls.Add(this.lblFirstName);
+            this.grpDetails.Location = new System.Drawing.Point(13, 13);
+            this.grpDetails.Name = "grpDetails";
+            this.grpDetails.Size = new System.Drawing.Size(628, 209);
+            this.grpDetails.TabIndex = 0;
+            this.grpDetails.TabStop = false;
+            this.grpDetails.Text = "Register New User:";
+            // 
+            // grbSex
+            // 
+            this.grbSex.Controls.Add(this.rdMale);
+            this.grbSex.Controls.Add(this.rdFamele);
+            this.grbSex.Location = new System.Drawing.Point(36, 117);
+            this.grbSex.Name = "grbSex";
+            this.grbSex.Size = new System.Drawing.Size(268, 86);
+            this.grbSex.TabIndex = 8;
+            this.grbSex.TabStop = false;
+            this.grbSex.Text = "Sex";
+            // 
+            // rdMale
+            // 
+            this.rdMale.AutoSize = true;
+            this.rdMale.Location = new System.Drawing.Point(139, 38);
+            this.rdMale.Name = "rdMale";
+            this.rdMale.Size = new System.Drawing.Size(48, 17);
+            this.rdMale.TabIndex = 1;
+            this.rdMale.TabStop = true;
+            this.rdMale.Text = "Male";
+            this.rdMale.UseVisualStyleBackColor = true;
+            // 
+            // rdFamele
+            // 
+            this.rdFamele.AutoSize = true;
+            this.rdFamele.Location = new System.Drawing.Point(19, 38);
+            this.rdFamele.Name = "rdFamele";
+            this.rdFamele.Size = new System.Drawing.Size(59, 17);
+            this.rdFamele.TabIndex = 0;
+            this.rdFamele.TabStop = true;
+            this.rdFamele.Text = "Female";
+            this.rdFamele.UseVisualStyleBackColor = true;
+            // 
+            // txtDepartment
+            // 
+            this.txtDepartment.Location = new System.Drawing.Point(360, 62);
+            this.txtDepartment.Name = "txtDepartment";
+            this.txtDepartment.Size = new System.Drawing.Size(100, 20);
+            this.txtDepartment.TabIndex = 7;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(360, 25);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 6;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(94, 62);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(100, 20);
+            this.txtLastName.TabIndex = 5;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(94, 26);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstName.TabIndex = 4;
+            // 
+            // lblDepartment
+            // 
+            this.lblDepartment.AutoSize = true;
+            this.lblDepartment.Location = new System.Drawing.Point(289, 62);
+            this.lblDepartment.Name = "lblDepartment";
+            this.lblDepartment.Size = new System.Drawing.Size(62, 13);
+            this.lblDepartment.TabIndex = 3;
+            this.lblDepartment.Text = "Department";
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(286, 34);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(18, 13);
+            this.lblID.TabIndex = 2;
+            this.lblID.Text = "ID";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(33, 62);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(58, 13);
+            this.lblLastName.TabIndex = 1;
+            this.lblLastName.Text = "Last Name";
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(30, 34);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(57, 13);
+            this.lblFirstName.TabIndex = 0;
+            this.lblFirstName.Text = "First Name";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(670, 38);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(670, 92);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 2;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(670, 192);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(75, 23);
+            this.btnRegister.TabIndex = 3;
+            this.btnRegister.Text = "Register User";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // grdUsers
+            // 
+            this.grdUsers.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.grdUsers.HideSelection = false;
+            this.grdUsers.Location = new System.Drawing.Point(0, 258);
+            this.grdUsers.Name = "grdUsers";
+            this.grdUsers.Size = new System.Drawing.Size(800, 192);
+            this.grdUsers.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.grdUsers.TabIndex = 35;
+            this.grdUsers.UseCompatibleStateImageBehavior = false;
+            this.grdUsers.View = System.Windows.Forms.View.Details;
+            this.grdUsers.SelectedIndexChanged += new System.EventHandler(this.grdUsers_SelectedIndexChanged);
+            // 
+            // UsersView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.grdUsers);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.grpDetails);
+            this.Name = "UsersView";
+            this.Text = "Form1";
+            this.grpDetails.ResumeLayout(false);
+            this.grpDetails.PerformLayout();
+            this.grbSex.ResumeLayout(false);
+            this.grbSex.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpDetails;
+        private System.Windows.Forms.GroupBox grbSex;
+        private System.Windows.Forms.RadioButton rdMale;
+        private System.Windows.Forms.RadioButton rdFamele;
+        private System.Windows.Forms.TextBox txtDepartment;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.Label lblDepartment;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnRegister;
+        internal System.Windows.Forms.ListView grdUsers;
+    }
+}
+
---
+++ Практика/Билет 11 Практика/MVC-View/UsersView.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MVC_Model;
+using MVC_Controller;
+
+namespace MVC_View
+{
+    public partial class UsersView : Form, IUsersView
+    {
+        public UsersView()
+        {
+            InitializeComponent();
+        }
+        UsersController _controller;
+
+        #region Events raised  back to controller
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            this._controller.AddNewUser();
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            this._controller.RemoveUser();
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            this._controller.Save();
+        }
+
+        private void grdUsers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (this.grdUsers.SelectedItems.Count > 0)
+                this._controller.SelectedUserChanged(this.grdUsers.SelectedItems[0].Text);
+        }
+
+
+        #endregion
+
+        #region ICatalogView implementation
+
+        public void SetController(UsersController controller)
+        {
+            _controller = controller;
+        }
+
+        public void ClearGrid()
+        {
+            // Define columns in grid
+            this.grdUsers.Columns.Clear();
+
+            this.grdUsers.Columns.Add("Id",          150,  HorizontalAlignment.Left);
+            this.grdUsers.Columns.Add("First Name",  150,  HorizontalAlignment.Left);
+            this.grdUsers.Columns.Add("Last Name", 150,  HorizontalAlignment.Left);
+            this.grdUsers.Columns.Add("Department",  150,  HorizontalAlignment.Left);
+            this.grdUsers.Columns.Add("Sex",         50,   HorizontalAlignment.Left);
+
+            // Add rows to grid
+            this.grdUsers.Items.Clear();
+        }
+
+        public void AddUserToGrid(User usr)
+        {
+            ListViewItem parent;
+            parent = this.grdUsers.Items.Add(usr.ID);
+            parent.SubItems.Add(usr.FirstName);
+            parent.SubItems.Add(usr.LastName);
+            parent.SubItems.Add(usr.Department);
+            parent.SubItems.Add(Enum.GetName(typeof(User.SexOfPerson), usr.Sex));
+        }
+
+        public void UpdateGridWithChangedUser(User usr)
+        {
+            ListViewItem rowToUpdate = null;
+
+            foreach (ListViewItem row in this.grdUsers.Items)
+            {
+                if (row.Text == usr.ID)
+                {
+                    rowToUpdate = row;
+                }
+            }
+
+            if (rowToUpdate != null)
+            {
+                rowToUpdate.Text = usr.ID;
+                rowToUpdate.SubItems[1].Text = usr.FirstName;
+                rowToUpdate.SubItems[2].Text = usr.LastName;
+                rowToUpdate.SubItems[3].Text = usr.Department;
+                rowToUpdate.SubItems[4].Text = Enum.GetName(typeof(User.SexOfPerson), usr.Sex);
+            }
+        }
+
+        public void RemoveUserFromGrid(User usr)
+        {
+
+            ListViewItem rowToRemove = null;
+
+            foreach (ListViewItem row in this.grdUsers.Items)
+            {
+                if (row.Text == usr.ID)
+                {
+                    rowToRemove = row;
+                }
+            }
+
+            if (rowToRemove != null)
+            {
+                this.grdUsers.Items.Remove(rowToRemove);
+                this.grdUsers.Focus();
+            }
+        }
+
+        public string GetIdOfSelectedUserInGrid()
+        {
+            if (this.grdUsers.SelectedItems.Count > 0)
+                return this.grdUsers.SelectedItems[0].Text;
+            else
+                return "";
+        }
+
+        public void SetSelectedUserInGrid(User usr)
+        {
+            foreach (ListViewItem row in this.grdUsers.Items)
+            {
+                if (row.Text == usr.ID)
+                {
+                    row.Selected = true;
+                }
+            }
+        }
+
+        public string FirstName 
+        {
+            get { return this.txtFirstName.Text; }
+            set { this.txtFirstName.Text = value; }
+        }
+
+        public string LastName 
+        {
+            get { return this.txtLastName.Text; }
+            set { this.txtLastName.Text = value; }
+        }
+
+        public string ID
+        {
+            get { return this.txtID.Text; }
+            set { this.txtID.Text = value; }
+        }
+
+
+        public string Department 
+        {
+            get { return this.txtDepartment.Text; }
+            set { this.txtDepartment.Text = value; }
+        }
+
+        public User.SexOfPerson Sex
+        {
+            get
+            {
+                if (this.rdMale.Checked)
+                    return User.SexOfPerson.Male;
+                else
+                    return User.SexOfPerson.Female;
+            }
+            set
+            {
+                if (value == User.SexOfPerson.Male)
+                    this.rdMale.Checked = true;
+                else
+                    this.rdFamele.Checked = true;
+            }
+        }
+
+        public bool CanModifyID
+        {
+            set { this.txtID.Enabled = value; }
+        }
+
+        #endregion
+
+      
+    }
+}
